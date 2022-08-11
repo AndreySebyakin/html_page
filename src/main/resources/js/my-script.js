570e96a9f74f18b87a42fbf4e2b89a1fd81b08dc
Andrey Sebyakin <andrey.sebyakin.by@gmail.com>
2022-08-11 09:04:06 +0300

    I created the page using the following tools: HTML, CSS, JS.

---
+++ src/main/resources/js/my-script.js
@@ -0,0 +1,15 @@
+function createExl() {
+alert("Excel file is ready");
+}
+
+function createPpt() {
+alert("PPT file is ready");
+}
+
+function calculate() {
+alert("Result is ready");
+}
+
+function cleanFilters() {
+alert("All filters cleared");
+}
